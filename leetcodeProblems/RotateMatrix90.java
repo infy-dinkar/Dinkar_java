a8394a4abfba2a3048b28b8fa354ce97061ae1ad
Dinkar Thakur <322dinkar@gmail.com>
2024-08-27 17:18:16 +0530

    2dArray

---
+++ leetcodeProblems/RotateMatrix90.java
@@ -0,0 +1,21 @@
+package leetcodeProblems;
+
+public class RotateMatrix90 {
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1, 2, 3},
+            {4,5,6},
+            {7,8,9}
+        };
+        for(int i=0;i<3;i++){
+            for(int j=i+1;j<3;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                temp=arr[j][i];
+
+            }
+        }
+
+    }
+    
+}
