f1b82e96bc1901a6802f2a4f3765e81f9ad1010e
Dinkar Thakur <322dinkar@gmail.com>
2024-11-25 23:23:31 +0530

    recursion

--- DSA_PROBLEMS/Print1toNwithoutUsingLoopGfg.java
+++ DSA_PROBLEMS/Print1toNwithoutUsingLoopGfg.java
@@ -2,9 +2,9 @@ package DSA_PROBLEMS;
 
 public class Print1toNwithoutUsingLoopGfg {
     class Solution{
-        static void printtillN(int N){
+        static void printTillN(int N){
             if (N==0) return;
-            printtillN(N-1);
+            printTillN(N-1);
             System.out.print(N+" ");
             
         }
