10443d0eaec27a0f756e9da1a23da057bfe93fd0
Dinkar Thakur <322dinkar@gmail.com>
2024-11-29 15:32:36 +0530

    BinarySearch

---
+++ DSA_PROBLEMS/SearchInRotatedSortedArrayLeetcode33.java
@@ -0,0 +1,27 @@
+package DSA_PROBLEMS;
+
+public class SearchInRotatedSortedArrayLeetcode33 {
+    class Solution {
+        public int search(int[] nums, int target) {
+            int n=nums.length;
+            int lo=0;
+            int hi=n-1;
+            while(lo<=hi){
+                int mid=lo+(hi-lo)/2;
+                if(nums[mid]==target) return mid;
+                if(nums[lo]<=nums[mid]){
+                    if ( nums[lo]<=target && target<=nums[mid]) hi=mid-1;
+                    else lo=mid+1;
+                }
+                else{
+                    if(nums[mid]<=target && target<=nums[hi]) lo=mid+1;
+                    else hi=mid-1;
+                }
+            }
+            return -1;
+            
+            
+        }
+    }
+    
+}
