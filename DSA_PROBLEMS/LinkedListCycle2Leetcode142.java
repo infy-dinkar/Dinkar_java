45a543eaaaacea48a94e8eb37a9657865f8efd6d
Dinkar Thakur <322dinkar@gmail.com>
2024-12-23 19:54:07 +0530

    linkedlist

---
+++ DSA_PROBLEMS/LinkedListCycle2Leetcode142.java
@@ -0,0 +1,40 @@
+public class LinkedListCycle2Leetcode142 {
+    /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                ListNode s1 = head;
+                ListNode s2 = slow;
+
+                while (s1 != s2) {
+                    s1 = s1.next;
+                    s2 = s2.next;
+                }
+
+                return s1;
+            }
+        }
+
+        return null;
+    }
+}
+
+    
+}
