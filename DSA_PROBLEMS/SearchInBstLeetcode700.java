6b434948574586d5f949d75037a2694dccc2ef13
Dinkar Thakur <322dinkar@gmail.com>
2025-01-16 20:09:43 +0530

    bst

---
+++ DSA_PROBLEMS/SearchInBstLeetcode700.java
@@ -0,0 +1,19 @@
+import javax.swing.tree.TreeNode;
+
+public class SearchInBstLeetcode700 {
+    class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+
+        while (root != null) {
+            if (root.val == val) return root;
+            if (root.val < val) root = root.right;
+            else root = root.left;
+        }
+
+        return null;
+    }
+}
+
+    
+}
