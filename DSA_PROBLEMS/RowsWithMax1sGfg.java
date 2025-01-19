701cf76d07a08676e2dcf8ee2008d2057cf105ce
Dinkar Thakur <322dinkar@gmail.com>
2025-01-19 19:26:57 +0530

    corrections

--- DSA_PROBLEMS/RowsWithMax1sGfg.java
+++ DSA_PROBLEMS/RowsWithMax1sGfg.java
@@ -5,16 +5,16 @@ public class RowsWithMax1sGfg {
         public int rowWithMax1s(int arr[][]) {
             int rows = arr.length;
             int cols = arr[0].length;
-            int p1 = 0;
-            int p2 = cols - 1;
+            int pt1 = 0;
+            int pt2 = cols - 1;
             int ans = -1;
     
-            while (p1 < rows && p2 >= 0) {
-                if (arr[p1][p2] == 1) {
-                    ans = p1;
-                    p2--;
+            while (pt1 < rows && pt2 >= 0) {
+                if (arr[pt1][pt2] == 1) {
+                    ans = pt1;
+                    pt2--;
                 } else {
-                    p1++;
+                    pt1++;
                 }
             }
     
