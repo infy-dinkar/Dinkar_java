0ba96a88c573f261712d6f84f0723b380dcf4d45
Dinkar Thakur <322dinkar@gmail.com>
2025-08-12 16:41:47 +0530

    stacks

---
+++ DSA_PROBLEMS/Leetcode503.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Leetcode503 {
+
+    class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        
+        for (int i = 0; i < 2 * n; i++) {
+            int idx = i % n;
+            if (st.size() == 0) {
+                st.push(idx);
+            } else {
+                while (st.size() != 0 && nums[st.peek()] < nums[idx]) {
+                    hm.put(st.pop(), nums[idx]);
+                }
+                st.push(idx);
+            }
+        }
+
+        while (st.size() != 0) {
+            int idx = st.pop();
+            if (!hm.containsKey(idx)) {
+                hm.put(idx, -1);
+            }
+        }
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = hm.get(i);
+        }
+
+        return res;
+    }
+}
+
+    
+}
