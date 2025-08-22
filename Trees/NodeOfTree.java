46c93fb3177d9b58a9fe2e67156169b8991ee725
Dinkar Thakur <322dinkar@gmail.com>
2025-08-22 17:58:48 +0530

    tree-making

---
+++ Trees/NodeOfTree.java
@@ -0,0 +1,35 @@
+package Trees;
+class Node{
+    int val;
+    Node left;
+    Node right;
+    Node(int val){
+        this.val=val;
+    }
+}
+
+
+public class NodeOfTree {
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        Node a=new Node(2);
+        Node b=new Node(3);
+        Node c=new Node(6);
+        Node d=new Node(9);
+        Node e= new Node(10);
+
+        root.left=a;
+        root.right=b;
+
+        a.left=c;
+        a.right=d;
+
+        b.left=e;
+
+
+
+
+        
+    }
+    
+}
