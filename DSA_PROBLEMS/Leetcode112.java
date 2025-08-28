7fb1f15c32c4d73588099b6cc44496897ed64a03
Dinkar Thakur <322dinkar@gmail.com>
2025-08-28 18:02:26 +0530

    trees

---
+++ DSA_PROBLEMS/Leetcode112.java
@@ -0,0 +1,35 @@
+import javax.swing.tree.TreeNode;
+
+public class Leetcode112 {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null) return false;
+        if(root.left==null && root.right==null){
+            if(targetSum==root.val) return true;
+        }
+
+        boolean l=hasPathSum(root.left,targetSum-root.val);
+        boolean r=hasPathSum(root.right,targetSum-root.val);
+
+        if(l|| r) return true;
+         return false;
+        
+    }
+}
+    
+}
