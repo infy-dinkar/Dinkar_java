4601774c235a8e2150db2004fb2dea92fe939b5e
Dinkar Thakur <322dinkar@gmail.com>
2025-09-08 21:16:50 +0530

    potd-copied solution

---
+++ DSA_PROBLEMS/Leetcode1317.java
@@ -0,0 +1,27 @@
+public class Leetcode1317 {
+    class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        for (int i = 1; i < n; i++) {
+            int a = i;
+            int b = n - i;
+            
+            if (noZero(a) && noZero(b)) {
+                return new int[]{a, b};
+            }
+        }
+        return new int[]{-1, -1};  
+    }
+    
+    private boolean noZero(int num) {
+        while (num > 0) {
+            if (num % 10 == 0) {
+                return false;
+            }
+            num /= 10;
+        }
+        return true;
+    }
+}
+
+    
+}
