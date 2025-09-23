0a2da86c8e91ce6ad7c9807a3036398ed6c931d0
Dinkar Thakur <322dinkar@gmail.com>
2025-09-23 22:39:12 +0530

    potd

---
+++ DSA_PROBLEMS/Leetcode165.java
@@ -0,0 +1,41 @@
+public class Leetcode165 {
+    class Solution {
+    public int compareVersion(String version1, String version2) {
+        
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        
+        int n = Math.max(v1.length, v2.length);
+
+        
+        for (int i = 0; i < n; i++) {
+            int num1, num2;
+
+            if (i < v1.length) {
+                num1 = Integer.parseInt(v1[i]);
+            } else {
+                num1 = 0;
+            }
+
+            if (i < v2.length) {
+                num2 = Integer.parseInt(v2[i]);
+            } else {
+                num2 = 0;
+            }
+
+            if (num1 > num2) {
+                return 1;
+            } else if (num1 < num2) {
+                return -1;
+            }
+            
+        }
+
+        
+        return 0;
+    }
+}
+
+    
+}
