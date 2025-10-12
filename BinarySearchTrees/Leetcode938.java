6517ceb88bde891d52a163f25c5adef430ea3f3a
Dinkar Thakur <322dinkar@gmail.com>
2025-10-12 20:25:30 +0530

    BST

---
+++ BinarySearchTrees/Leetcode938.java
@@ -0,0 +1,25 @@
+package BinarySearchTrees;
+
+import javax.swing.tree.TreeNode;
+
+public class Leetcode938 {
+    class Solution {
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+
+        if (root.val >= low && root.val <= high)
+            sum += root.val;
+
+        if (root.val > low)
+            rangeSumBST(root.left, low, high);
+
+        if (root.val < high)
+            rangeSumBST(root.right, low, high);
+
+        return sum;
+    }
+}
+
+    
+}
